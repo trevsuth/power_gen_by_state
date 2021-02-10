37ac7aacbc106a5396ac065f165fed8e2636760e
Trevor Sutherland <trevor.sutherland@gmail.com>
2021-02-09 19:59:20 -0500

    Added Plotly elements

--- GeneratorMap.R
+++ GeneratorMap.R
@@ -20,22 +20,30 @@ state <- 'KY'
 OperatingData <- read_excel(doc, OperatingSheet) 
 OperatingData <- OperatingData %>% 
   filter(`Plant State` == state) %>% 
-  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology) %>%
+  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology, `Plant Name`) %>%
   as.data.frame() %>%
   usmap_transform()
 
 PlannedData <- read_excel(doc, PlannedSheet) 
 PlannedData <- PlannedData %>% 
   filter(`Plant State` == state) %>% 
-  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology) %>%
+  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology, `Plant Name`) %>%
   as.data.frame() %>%
   usmap_transform()
 
 RetiredData <- read_excel(doc, RetiredSheet) 
 RetiredData <- RetiredData %>%
   filter(`Plant State` == state) %>% 
-  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology) %>%
+  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology, `Plant Name`) %>%
   as.data.frame() %>%
   usmap_transform()
 
-plot_usmap("counties", include = c(state)) + geom_point(data=OperatingData, aes(x=Longitude.1, y=Latitude.1, color=Technology))
+ggplotly(
+plot_usmap("counties", include = c(state)) + 
+  geom_point(data=OperatingData, 
+             aes(x=Longitude.1, 
+                 y=Latitude.1, 
+                 color=Technology
+                 )
+  )
+)
