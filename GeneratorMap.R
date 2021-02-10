0a039012d5386dd37ae166bdb00388d71b02940d
Trevor Sutherland <trevor.sutherland@gmail.com>
2021-02-09 19:52:36 -0500

    Basic generator map now working

--- GeneratorMap.R
+++ GeneratorMap.R
@@ -13,6 +13,29 @@ OperatingSheet <- 'Operating'
 PlannedSheet <- 'Planned'
 RetiredSheet <- 'Retired'
 
-OperatingData <- read_excel(doc, OperatingSheet, skip=2) 
+#set state
+state <- 'KY'
 
-OperatingData <- OperatingData %>% filter(`Plant State` != '')
+#Create data sets
+OperatingData <- read_excel(doc, OperatingSheet) 
+OperatingData <- OperatingData %>% 
+  filter(`Plant State` == state) %>% 
+  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology) %>%
+  as.data.frame() %>%
+  usmap_transform()
+
+PlannedData <- read_excel(doc, PlannedSheet) 
+PlannedData <- PlannedData %>% 
+  filter(`Plant State` == state) %>% 
+  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology) %>%
+  as.data.frame() %>%
+  usmap_transform()
+
+RetiredData <- read_excel(doc, RetiredSheet) 
+RetiredData <- RetiredData %>%
+  filter(`Plant State` == state) %>% 
+  select(Longitude, Latitude, `Nameplate Capacity (MW)`, Technology) %>%
+  as.data.frame() %>%
+  usmap_transform()
+
+plot_usmap("counties", include = c(state)) + geom_point(data=OperatingData, aes(x=Longitude.1, y=Latitude.1, color=Technology))
